98d1ef83fd20e35665fea248206bc6a129ef26cd
Justin Handville <nanolith@gmail.com>
2021-11-12 04:03:23 -0500

    Extended IT to verify the next prev block id.

--- include/helpers/conn_helpers.h
+++ include/helpers/conn_helpers.h
@@ -104,6 +104,27 @@ status get_and_verify_next_block_id(
     const vpr_uuid* block_id, vpr_uuid* next_block_id);
 
 /**
+ * \brief Request the prev block ID for a given block ID from the agentd
+ * instance.
+ *
+ * \param sock              The socket connection with agentd.
+ * \param suite             The crypto suite to use for this operation.
+ * \param client_iv         The client-side initialization vector counter.
+ * \param server_iv         The server-side initialization vector counter.
+ * \param shared_secret     The computed shared secret for this session.
+ * \param block_id          The block id to query.
+ * \param prev_block_id     Variable to hold the prev block id on success.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+status get_and_verify_prev_block_id(
+    ssock* sock, vccrypt_suite_options_t* suite, uint64_t* client_iv,
+    uint64_t* server_iv, vccrypt_buffer_t* shared_secret,
+    const vpr_uuid* block_id, vpr_uuid* prev_block_id);
+
+/**
  * \brief Request a block by ID from the agentd instance.
  *
  * \param sock              The socket connection with agentd.
