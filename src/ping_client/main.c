b513477bf5e3438a4a6513e8d4c5ac28faf4d7d2
Justin Handville <nanolith@gmail.com>
2022-07-18 10:04:25 -0400

    ping client stub.

---
+++ src/ping_client/main.c
@@ -0,0 +1,23 @@
+/**
+ * \file ping_client/main.c
+ *
+ * \brief Main entry point for the ping client test utility.
+ *
+ * \copyright 2022 Velo Payments.  See License.txt for license terms.
+ */
+
+/**
+ * \brief Main entry point for the ping client test utility.
+ *
+ * \param argc      The number of arguments.
+ * \param argv      Arguments to main.
+ *
+ * \returns 0 on success and non-zero on failure.
+ */
+int main(int argc, char* argv[])
+{
+    (void)argc;
+    (void)argv;
+
+    return -1;
+}
